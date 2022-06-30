e2cab979927c752daafd2ed7d8d04e003d37e9fb
ahmetercek <ercek.ahmett@gmail.com>
2022-06-30 22:56:24 +0300

    Hilt dependency injection library implemented.

---
+++ DemoProject/app/src/main/java/com/example/seavusdemoproject/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.example.seavusdemoproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
--- DemoProject/app/src/main/java/com/example/seavusdemoproject/MainActivity.kt
+++ DemoProject/app/src/main/java/com/example/seavusdemoproject/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.seavusdemoproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG : String = "MainActivity_DEBUG_TAG"
+
+    @Inject
+    lateinit var application: BaseApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "App package name is: ${application.packageName}")
     }
 }
