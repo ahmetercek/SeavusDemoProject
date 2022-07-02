20b5e1f48411b2e2c72bcd6eda326dd3421f0069
ahmetercek <ercek.ahmett@gmail.com>
2022-07-02 14:26:21 +0300

    Added search by user name feature to recyclerView .

--- DemoProject/app/src/main/java/com/example/seavusdemoproject/presentation/ui/user_list/UserListFragment.kt
+++ DemoProject/app/src/main/java/com/example/seavusdemoproject/presentation/ui/user_list/UserListFragment.kt
@@ -1,17 +1,21 @@
 package com.example.seavusdemoproject.presentation.ui.user_list
 
 import android.os.Bundle
-import android.util.Log
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.seavusdemoproject.R
 import com.example.seavusdemoproject.presentation.adapter.UserListRecyclerViewAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import android.text.Editable
+import com.example.seavusdemoproject.domain.model.User
+
 
 @AndroidEntryPoint
 class UserListFragment : Fragment() {
@@ -22,8 +26,11 @@ class UserListFragment : Fragment() {
 
     private val viewModel: UserListViewModel by viewModels()
 
+    private lateinit var editText: EditText
     private lateinit var recyclerView: RecyclerView
     private lateinit var recyclerViewAdapter: UserListRecyclerViewAdapter
+    private lateinit var userList: List<User>
+    private lateinit var filteredList: List<User>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +45,8 @@ class UserListFragment : Fragment() {
         initViews()
 
         viewModel.users.observe(viewLifecycleOwner) {
-            recyclerViewAdapter.setData(it)
+            userList = it
+            recyclerViewAdapter.setData(userList)
         }
         viewModel.fetchUsers()
     }
@@ -51,6 +59,27 @@ class UserListFragment : Fragment() {
             adapter = recyclerViewAdapter
         }
 
+        editText = requireView().findViewById<EditText>(R.id.edit_text)
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+            override fun afterTextChanged(s: Editable) {
+                searchUser(s.toString())
+            }
+        })
+    }
+
+    private fun searchUser(query: String) {
+        if(query.isNullOrEmpty()){
+            recyclerViewAdapter.setData(userList)
+            return
+        }
+        val filteredList: List<User> = userList.filter{ user -> user.name.lowercase().contains(query.lowercase()) }.toList()
+        recyclerViewAdapter.setData(filteredList)
     }
 }
 
